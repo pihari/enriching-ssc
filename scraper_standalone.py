5463941b2b11ddd96986b63f537562cd245d3be7
Pierre Haritz <pierre.haritz@udo.edu>
2021-03-08 09:28:01 +0100

    added standalone scraper

---
+++ scraper_standalone.py
@@ -0,0 +1,29 @@
+import json
+import os
+import sys
+import git
+import requests
+
+def main_scraper():
+    with open('links/links-between-papers-and-code.json') as f:
+        data = json.load(f)
+        for i in range(len(data)):
+            url = data[i]['repo_url']
+            aid = data[i]['paper_arxiv_id']
+            if aid is not None and aid.startswith('1901'):
+                path = os.path.join("repos", aid)
+                if not os.path.isdir(path):
+                    print(f"Cloning {url} into {path}")
+                    try:
+                        git.Repo.clone_from(url, path)
+                    except git.exc.GitError:
+                        print(f'ERROR! {url} does not exist')
+                tex_path = os.path.join("1901 pwc", aid)
+                if not os.path.isdir(tex_path):
+                    print(f"Downloading {aid} from arXiv")
+                    a_url = f"https://arxiv.org/e-print/{aid}"
+                    r = requests.get(a_url, allow_redirects=True)
+                    open(f'{aid}', 'wb').write(r.content)
+
+if __name__=="__main__":
+    main_scraper()
