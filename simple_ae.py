bc7b72cd7733c843a4ba5eb8ebe9934939c8822e
pihari <pierre.haritz@udo.edu>
2021-04-09 10:38:13 +0200

    tweaks to img output

--- simple_ae.py
+++ simple_ae.py
@@ -182,11 +182,12 @@ class DataVisualizer:
         loss_p_data = [d[0] for d in self.data]
         loss_c_data = [d[1] for d in self.data]
         loss_cse = [d[2] for d in self.data]
-        loss_ae = loss_p_data + loss_c_data + loss_cse
+        loss_ae = [a+b+c for (a,b,c) in zip(loss_p_data, loss_c_data, loss_cse)]
+        print(self.data)
         fig = plt.figure()
-        plt.plot(loss_p_data, len(loss_p_data)*[1])
-        plt.plot(loss_c_data, len(loss_c_data)*[1])
-        plt.plot(loss_cse, len(loss_cse)*[1])
+        #plt.plot(loss_p_data, len(loss_p_data)*[1])
+        #plt.plot(loss_c_data, len(loss_c_data)*[1])
+        #plt.plot(loss_cse, len(loss_cse)*[1])
         plt.plot(loss_ae, len(loss_ae)*[1])
         plt.autoscale()
         fig.savefig(f'./outimgs/loss_'+str({i}))
